26f952cada88e57ae301f8251dd44f19868a3d3c
Mikhail Kostrikov <kostrikovma@vk.com>
2024-10-20 17:21:32 +0300

    add tabs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,17 @@
-import menu from "./menu.js";
+import menu from "./menuModule.js";
+import tabsModule from "./tabsModule.js";
 
 menu.addEvents({
   dialog: document.querySelector(".js-menu"),
   openBtn: document.querySelector(".js-menu-open"),
   closeBtn: document.querySelector(".js-menu-close"),
+});
+
+tabsModule.init({
+  navs: [
+    ...document.querySelector(".js-tabs").querySelectorAll(".js-tabs-nav"),
+  ],
+  slides: [
+    ...document.querySelector(".js-tabs").querySelectorAll(".js-tabs-slide"),
+  ],
 });
